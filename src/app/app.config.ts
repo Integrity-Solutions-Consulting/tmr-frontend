c1e7c71784d11cef3a83143be7cd13ed554d9a54
Javier Luna <javier.luna@integritysolutions.com.ec>
2026-05-13 14:32:30 -0500

    http inteceptor mock para simular apis de backend

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, HTTP_INTERCEPTORS, withInterceptorsFromDi } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { AuthMockInterceptor } from './core/interceptors/auth-mock.interceptor';
+import { authReducer } from './features/auth/store/auth.reducer';
+import { AuthEffects } from './features/auth/store/auth.effects';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideStore({ auth: authReducer }),
+    provideEffects([AuthEffects]),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthMockInterceptor, multi: true }
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { authRoutes } from './features/auth/auth.routes';
+import { AppLayout } from './core/layout/app-layout/app-layout';
+import { ReporteFechasComponent } from './features/reportes/componentes/reporte-fechas/reporte-fechas.component';
+import { TablaEjemplosComponent } from './shared/componentes/tabla-ejemplos';
 
-export const routes: Routes = [];
+// Routes configuration
+export const routes: Routes = [
+  {
+    path: 'auth',
+    children: authRoutes,
+  },
+  {
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full',
+  },
+/*  { 
+    path: 'appMenu',
+    component: AppLayout,
+  },
+  {
+    path: 'colaboradores',
+    loadChildren: () =>
+      import('./features/colaboradores/colaboradores.routes').then(
+        m => m.COLABORADORES_ROUTES
+      ),
+  },
+  {
+    path: 'reportes',
+    component: ReporteFechasComponent,
+  },
+  {
+    path: 'tabla-ejemplos',
+    component: TablaEjemplosComponent,
+  },
+
+*/
+  
+];
