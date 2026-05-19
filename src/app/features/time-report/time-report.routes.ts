94465d10148ecf8b028e5f17a638e43c7ce5fcd4
Tommy Murillo <tommyamr@hotmail.com>
2026-05-19 16:23:59 -0500

    update de feature seguimiento: se realiazo cambios en los filtros, badge estados y el diseño

--- src/app/features/time-report/time-report.routes.ts
+++ src/app/features/time-report/time-report.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { Actividades } from './actividades/actividades';
-import { SeguimientoComponent } from './seguimiento/seguimiento.component';
+import { SeguimientoComponent } from './seguimiento/seguimiento';
 
 export const TIME_REPORT_ROUTES: Routes = [
   { path: 'actividades', component: Actividades },
