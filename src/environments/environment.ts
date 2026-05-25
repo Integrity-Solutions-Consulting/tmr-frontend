2b447d34a2b4a7438209281219324bdd85bb2024
PaulinaAnz25 <paulinaanzules25@gmail.com>
2026-05-25 00:44:16 -0500

    Actualizar environment para colaboradores

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   // URL base del backend (puerto 5071, http)
   apiUrl: 'http://localhost:5071/api',
-  tokenPruebas: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzIiwiZW1haWwiOiJwcnVlYmFAdGVzdDIuY29tIiwiaWF0IjoxNzc5Njc2MjIwLCJleHAiOjE3Nzk2NzcxMjAsImlzcyI6InRtci1iYWNrZW5kIiwiYXVkIjoidG1yLXVzdWFyaW9zIn0.tjDrLZks6hBx5d6cJPuo2lQZLVUUHO8XH3k69lRE_5I'   // el token que generes en Scalar
+  tokenPruebas: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0IiwiZW1haWwiOiJwcnVlYmEzQHRlc3QuY29tIiwiaWF0IjoxNzc5Njc5NTQ0LCJleHAiOjE3Nzk2OTc1NDQsImlzcyI6InRtci1iYWNrZW5kIiwiYXVkIjoidG1yLXVzdWFyaW9zIn0.EfizjyhZaj15j1LwvzjurCD5ZR49tVeylEL_iEzQ2QQ'   // el token que generes en Scalar
 };
