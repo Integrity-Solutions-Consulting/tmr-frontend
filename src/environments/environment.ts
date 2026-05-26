b13eee0cb39a6880c56c01975a007bff0e860216
PaulinaAnz25 <paulinaanzules25@gmail.com>
2026-05-26 02:00:34 -0500

    enviroment prueba con token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   // URL base del backend (puerto 5071, http)
   apiUrl: 'http://localhost:5071/api',
-  tokenPruebas: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0IiwiZW1haWwiOiJwcnVlYmEzQHRlc3QuY29tIiwiaWF0IjoxNzc5Njc5NTQ0LCJleHAiOjE3Nzk2OTc1NDQsImlzcyI6InRtci1iYWNrZW5kIiwiYXVkIjoidG1yLXVzdWFyaW9zIn0.EfizjyhZaj15j1LwvzjurCD5ZR49tVeylEL_iEzQ2QQ'   // el token que generes en Scalar
+  tokenPruebas: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5IiwiZW1haWwiOiJwcnVlYmE3QHRlc3QuY29tIiwiaWF0IjoxNzc5Nzc0MTUyLCJleHAiOjE3Nzk3OTIxNTIsImlzcyI6InRtci1iYWNrZW5kIiwiYXVkIjoidG1yLXVzdWFyaW9zIn0.fnAPQkKsrmISgMjh9N1Hb0vK-TIN2s84W_RM14Lw4GY'   
 };
