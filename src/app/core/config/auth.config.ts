a0858bf5f2bc237c2d6765d76b28dd6036378e23
Elisa Rodríguez <elisaholguin21@gmail.com>
2026-05-28 12:15:58 -0500

    Modificacion de Auth.confg.ts

--- src/app/core/config/auth.config.ts
+++ src/app/core/config/auth.config.ts
@@ -8,8 +8,8 @@ export const AUTH_CONFIG = {
   TOKEN_KEY: 'authToken',
 
   // Token JWT válido para desarrollo
-  //TOKEN_VALUE: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxNSIsImVtYWlsIjoibWFyaWJlbEBnbWFpbC5jb20iLCJpYXQiOjE3Nzk5MDc2MjAsImV4cCI6MTc3OTkxMTIyMCwiaXNzIjoidG1yLWJhY2tlbmQiLCJhdWQiOiJ0bXItdXN1YXJpb3MifQ.HDbViTON9C4gIccGK8DFHTvfGaUKMGMqwausREkSwdw',
-  TOKEN_VALUE: localStorage.getItem('token') || '',
+  TOKEN_VALUE: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxNSIsImVtYWlsIjoibWFyaWJlbEBnbWFpbC5jb20iLCJpYXQiOjE3Nzk5MDc2MjAsImV4cCI6MTc3OTkxMTIyMCwiaXNzIjoidG1yLWJhY2tlbmQiLCJhdWQiOiJ0bXItdXN1YXJpb3MifQ.HDbViTON9C4gIccGK8DFHTvfGaUKMGMqwausREkSwdw',
+  //TOKEN_VALUE: localStorage.getItem('authToken') || '',
   // URL base de la API de autenticación
   API_URL: 'http://localhost:5071/api',
 
