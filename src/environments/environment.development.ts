2000fc930e79b26f8a221e2da4c4c92e45f97af4
Tommy Murillo <tommy.murillo@tu-correo.com>
2026-06-04 14:21:39 -0500

    conexion del frontend tmr

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  // Esta es la URL base de tu backend .NET (sin el /scalar)
+  // Si tus endpoints tienen el prefijo /api (ej: /api/actividades), puedes poner 'http://localhost:5091/api'
+  apiUrl: 'http://localhost:5091/api'
+};
